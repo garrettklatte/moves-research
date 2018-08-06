ac04200f5c9dd208916eeef851079a7bbac8895c
EC2 Default User <ec2-user@ip-172-31-83-117.ec2.internal>
2018-08-06 04:16:21 +0000

    create log of spacetime points

--- weather.py
+++ weather.py
@@ -1,36 +1,78 @@
+"""Utility for retrieving weather summary information."""
+
 import argparse
-import urllib
-import re
-import time
 import csv
-import os
 from collections import namedtuple
-import json
 from datetime import date
+import requests
 
 WeatherSummary = namedtuple('WeatherSummary', 'mean_temp max_temp min_temp precipitation')
 SpacetimePoint = namedtuple('SpacetimePoint', 'latitude longitude date')
 
 API_KEY = '88e2f56333477b74'
 
-def _make_history_request(api_key, point):
-    return 'http://api.wunderground.com/api/{0}/history_{1}/q/{2},{3}.json'.format(
-        api_key,
-        point.date.strftime('%Y%m%d'),
-        point.latitude,
-        point.longitude)
+class WeatherUndergroundGateway:
+    @staticmethod
+    def fetch_weather_summary(spacetime_point, api_key=API_KEY):
+        """Using 'api_key' for authorization, fetch a summary of the weather
+        at 'spacetime_point'.
+        """
+        history_request = WeatherUndergroundGateway._make_history_request(api_key, spacetime_point)
+        history_response = requests.get(history_request).json()
+        return WeatherUndergroundGateway._pluck_history_response(history_response)
 
-def _pluck_history_response(response):
-    data = json.load(response)['history']['dailysummary'][0]
+    @staticmethod
+    def fetch_weather_data(spacetime_point, api_key=API_KEY):
+        """Using 'api_key' for authorization, fetch all weather data at 'spacetime_point."""
+        request = WeatherUndergroundGateway._make_history_request(api_key, spacetime_point)
+        response = requests.get(request).text
+        return response
 
-    return WeatherSummary(
-        data['meantempi'],
-        data['maxtempi'],
-        data['mintempi'],
-        data['precipi'])
+    @staticmethod
+    def _make_history_request(api_key, point):
+        """Using 'api_key' for authorization, make a request for fetching weather
+        data at 'point'.
 
-def _send_request(request):
-    return urllib.urlopen(request)
+        >>> from datetime import date
+        >>> api_key = 123456
+        >>> point = SpacetimePoint(34.9, 109.2, date(2018, 1, 1))
+        >>> WeatherUndergroundGateway._make_history_request(api_key, point)
+        'http://api.wunderground.com/api/123456/history_20180101/q/34.9,109.2.json'
+        """
+        return 'http://api.wunderground.com/api/{api_key}/history_{date}/q/{latitude},{longitude}.json'.format(
+            api_key=api_key,
+            date=point.date.strftime('%Y%m%d'),
+            latitude=point.latitude,
+            longitude=point.longitude
+        )
+
+    @staticmethod
+    def _pluck_history_response(response):
+        """Pluck the WeatherSummary from 'response'.
+
+        >>> resp = {'history': {
+        ... 'dailysummary':
+        ... [{'meantempi': 70,
+        ... 'maxtempi': 80,
+        ... 'mintempi': 60,
+        ... 'precipi': 3
+        ... }]}}
+        >>> WeatherUndergroundGateway._pluck_history_response(resp)
+        WeatherSummary(mean_temp=70, max_temp=80, min_temp=60, precipitation=3)
+        """
+        data = response['history']['dailysummary'][0]
+
+        return WeatherSummary(
+            data['meantempi'],
+            data['maxtempi'],
+            data['mintempi'],
+            data['precipi']
+        )
+
+def extract_spacetime_point(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return _fetch_spacetime_point(next(reader))
 
 def _fetch_spacetime_point(data):
     d = data['Data.Date'][:10]
@@ -39,11 +81,6 @@ def _fetch_spacetime_point(data):
         data['Data.Longitude'],
         date(int(d[:4]), int(d[5:7]), int(d[8:10]))
     )
-
-def fetch_spacetime_point(filename):
-    with open(filename, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        return _fetch_spacetime_point(next(reader))
 
 def write_to_csv(weather_by_id_and_spacetime, filename):
     with open(filename, 'w') as csvfile:
@@ -70,15 +107,6 @@ def get_device_id(filename):
     """
     return filename.split('_')[0]
 
-def fetch_weather_summary(api_key, spacetime_point):
-    history_request = _make_history_request(
-        api_key,
-        spacetime_point)
-    
-    history_response = _send_request(history_request)
-    
-    return _pluck_history_response(history_response)
-
 def valid_file(filename):
     """Use this function to filter out files that should not be analyzed.
 
@@ -99,19 +127,25 @@ def valid_file(filename):
 
     return True
 
-if __name__ == '__main__':
-
+def log_spacetime_points():
     parser = argparse.ArgumentParser()
     parser.add_argument('out', help='name of the outputted csv file')
     parser.add_argument('files', nargs='+', help='files to be processed')
     args = parser.parse_args()
 
-    valid_files = [file for file in args.files if valid_file(file)]
+    valid_files = [filename for filename in args.files if valid_file(filename)]
 
-    id_and_spacetime_point_list = [(get_device_id(filename), fetch_spacetime_point(filename))
-                                   for filename in valid_files]
+    spacetime_points = [extract_spacetime_point(filename) for filename in valid_files]
 
-    weather_by_id_and_spacetime = {(id, point): fetch_weather_summary(API_KEY, point)
-                                   for id, point in id_and_spacetime_point_list}
+    with open(args.out, 'w') as out:
+        writer = csv.DictWriter(out, fieldnames=['latitude', 'longitude', 'date'])
+        writer.writeheader()
+        for spacetime_point in spacetime_points:
+            writer.writerow({
+                'latitude': spacetime_point.latitude,
+                'longitude': spacetime_point.longitude,
+                'date': spacetime_point.date.isoformat()
+            })
 
-    write_to_csv(weather_by_id_and_spacetime, args.out)
+if __name__ == '__main__':
+    log_spacetime_points()
