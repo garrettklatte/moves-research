236f77ce1f50e4ff1fe111644c823b306405c869
Garrett Klatte <gklatte@ucla.edu>
2018-09-17 03:29:32 +0000

    Add 'apparent'  temperatures as optional

--- definitions/weather_summary.py
+++ definitions/weather_summary.py
@@ -1,8 +1,14 @@
-from collections import namedtuple
+"""Module for weather_summary."""
 
-WeatherSummary = namedtuple('WeatherSummary', [
-    'mean_temp',
-    'max_temp',
-    'min_temp',
-    'precipitation'
-])
+from dataclasses import dataclass
+
+@dataclass
+class WeatherSummary:
+    """Class that represents a summary of the weather."""
+    mean_temp: float
+    max_temp: float
+    min_temp: float
+    precipitation: float
+    apparent_mean_temp: float = None
+    apparent_max_temp: float = None
+    apparent_min_temp: float = None
